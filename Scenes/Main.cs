5ea462b5a809096b668d598ed4d9023599dff66c
dfcut8 <dfcut8@proton.me>
2025-10-03 11:33:03 -0400

    snapping done

--- Scenes/Main.cs
+++ Scenes/Main.cs
@@ -4,10 +4,18 @@ namespace GridBasedPuzzle.Core;
 
 public partial class Main : Node2D
 {
+    private Sprite2D sprite;
+
+    public override void _Ready()
+    {
+        sprite = GetNode<Sprite2D>("%Cursor");
+    }
+
     public override void _Process(double delta)
     {
         var mousePosition = GetGlobalMousePosition();
         var gridPosition = (mousePosition / 64).Floor();
         GD.Print($"Grid Position: {gridPosition}");
+        sprite.GlobalPosition = gridPosition * 64;
     }
 }
