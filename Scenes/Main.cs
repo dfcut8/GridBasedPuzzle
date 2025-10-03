da66cc823dc95e6872276f13363976a436af39c1
dfcut8 <dfcut8@proton.me>
2025-10-03 11:44:45 -0400

    building placement works

--- Scenes/Main.cs
+++ Scenes/Main.cs
@@ -6,6 +6,8 @@ public partial class Main : Node2D
 {
     private Sprite2D sprite;
 
+    [Export] private PackedScene buildingScene;
+
     public override void _Ready()
     {
         sprite = GetNode<Sprite2D>("%Cursor");
@@ -13,9 +15,29 @@ public partial class Main : Node2D
 
     public override void _Process(double delta)
     {
-        var mousePosition = GetGlobalMousePosition();
-        var gridPosition = (mousePosition / 64).Floor();
-        GD.Print($"Grid Position: {gridPosition}");
+        var gridPosition = GetMouseGridCellPosition();
         sprite.GlobalPosition = gridPosition * 64;
+    }
+
+    public override void _UnhandledInput(InputEvent e)
+    {
+        if (e.IsActionPressed("mouse_click_left"))
+        {
+            PlaceBuildingAtMousePosition();
+        }
+    }
+
+    private Vector2 GetMouseGridCellPosition()
+    {
+        var mousePosition = GetGlobalMousePosition();
+        return (mousePosition / 64).Floor();
+    }
+
+    private void PlaceBuildingAtMousePosition()
+    {
+        var building = buildingScene.Instantiate<Node2D>();
+        AddChild(building);
+        var gridPosition = GetMouseGridCellPosition();
+        building.GlobalPosition = gridPosition * 64;
     }
 }
