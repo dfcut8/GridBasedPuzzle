fd3239115fe8faef36afcfa94047c66bb24853fe
dfcut8 <dfcut8@proton.me>
2025-10-28 11:38:52 -0400

    add zoom in / zoom out

--- Scenes/Camera.cs
+++ Scenes/Camera.cs
@@ -12,6 +12,12 @@ public partial class Camera : Camera2D
     private readonly string INPUT_CAMERA_PAN_RIGHT = "camera_pan_right";
     private readonly string INPUT_CAMERA_PAN_UP = "camera_pan_up";
     private readonly string INPUT_CAMERA_PAN_DOWN = "camera_pan_down";
+    private readonly string INPUT_CAMERA_ZOOM_OUT = "camera_zoom_out";
+    private readonly string INPUT_CAMERA_ZOOM_IN = "camera_zoom_in";
+    private float maxZoomIn = 3.0f;
+    private float maxZoomOut = 0.5f;
+    private float zoomStep = 0.25f;
+    private float currentZoom = 1.0f;
 
     public override void _Process(double delta)
     {
@@ -24,6 +30,8 @@ public partial class Camera : Camera2D
             INPUT_CAMERA_PAN_DOWN).Normalized();
 
         GlobalPosition += movement * cameraSpeed * (float)delta;
+
+        HandleZoomInput();
     }
 
     public void SetBoundariesRect(Rect2I rect)
@@ -37,5 +45,24 @@ public partial class Camera : Camera2D
     public void SetCameraPosition(Vector2 pos)
     {
         GlobalPosition = pos;
+    }
+
+    private void HandleZoomInput()
+    {
+        if (Input.IsActionJustPressed(INPUT_CAMERA_ZOOM_IN))
+        {
+            if (currentZoom > maxZoomOut)
+            {
+                currentZoom -= zoomStep;
+            }
+        }
+        else if (Input.IsActionJustPressed(INPUT_CAMERA_ZOOM_OUT))
+        {
+            if (currentZoom < maxZoomIn)
+            {
+                currentZoom += zoomStep;
+            }
+        }
+        Zoom = new Vector2(currentZoom, currentZoom);
     }
 }
