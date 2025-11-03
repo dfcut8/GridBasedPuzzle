2f9996b99afc553e73f769cf34fbe62826469b56
dfcut8 <dfcut8@proton.me>
2025-11-03 10:26:39 -0500

    add shortcut to skip level

--- Scenes/Managers/GlobalInputManager.cs
+++ Scenes/Managers/GlobalInputManager.cs
@@ -1,15 +1,22 @@
 using Godot;
+using GridBasedPuzzle.Autoloads;
 
 namespace GridBasedPuzzle.Managers;
 
 public partial class GlobalInputManager : Node
 {
     public const string GLOBAL_INPUT_STAGE_RESET = "global_stage_reset";
+    public const string GLOBAL_INPUT_STAGE_LOADNEXT = "global_stage_loadnext";
     public override void _Process(double delta)
     {
         if (Input.IsActionJustPressed(GLOBAL_INPUT_STAGE_RESET))
         {
             GetTree().ReloadCurrentScene();
+        }
+
+        if (Input.IsActionJustPressed(GLOBAL_INPUT_STAGE_LOADNEXT))
+        {
+            LevelManager.Instance.LoadNextLevel();
         }
     }
 }
