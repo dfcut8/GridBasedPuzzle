bfe10f8f476ae5ecaf12d82aa53591b31ef061ce
dfcut8 <dfcut8@proton.me>
2025-11-14 10:35:16 -0500

    trying offset

--- Scenes/Isometric.cs
+++ Scenes/Isometric.cs
@@ -1,6 +1,20 @@
 using Godot;
-using System;
 
 public partial class Isometric : Node2D
 {
+    [Export] private TileMapLayer tileMap;
+
+    public override void _Ready()
+    {
+        var tiles = tileMap.GetUsedCells();
+        if (tiles.Count > 0)
+        {
+            var randomTilePos = tiles[(int)(GD.Randi() % (uint)tiles.Count)];
+            var tileData = tileMap.GetCellTileData(randomTilePos);
+
+            GD.Print($"Random tile: {randomTilePos}");
+            GD.Print($"Tile data: {tileData}");
+            tileData.TextureOrigin += new Vector2I(0, 4);
+        }
+    }
 }
